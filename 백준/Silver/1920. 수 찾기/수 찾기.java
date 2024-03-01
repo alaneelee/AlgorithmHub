cf685cf6fb1d6e8f91625f3154f7182354d724c8
munee <121735319+alaneelee@users.noreply.github.com>
2024-03-01 15:39:07 +0900

    [Silver IV] Title: 수 찾기, Time: 736 ms, Memory: 55212 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,87 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static void input() throws Exception {
+        Map<Integer, Integer> nums = new HashMap<>();
+        int N = scan.nextInt();
+
+        for (int i = 0; i < N; i++) {
+            nums.put(scan.nextInt(), 1);
+        }
+
+        int M = scan.nextInt();
+
+        for (int i = 0; i < M; i++) {
+            sb.append(nums.getOrDefault(scan.nextInt(), 0)).append("\n");
+        }
+
+        print();
+    }
+
+    static void print() {
+        System.out.print(sb.toString());
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        input();
+    }
+
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+}
