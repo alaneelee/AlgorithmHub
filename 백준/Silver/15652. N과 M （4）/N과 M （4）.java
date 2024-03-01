61cf8787f96986b176181b8e885d2260578a92fc
munee <121735319+alaneelee@users.noreply.github.com>
2024-03-01 20:46:18 +0900

    [Silver III] Title: N과 M (4), Time: 212 ms, Memory: 17800 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,96 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static void input() throws Exception {
+        N = scan.nextInt();
+        M = scan.nextInt();
+
+        makeCombinations(1, 0, "");
+
+        print();
+    }
+
+    static void makeCombinations(int start, int count, String current) {
+
+        if (count == M) {
+            sb.append(current).append("\n");
+            return;
+        }
+
+        for (int i = start; i <= N; i++) {
+            makeCombinations(i, count + 1, current + i + " ");
+        }
+
+    }
+
+    static void print() {
+        System.out.print(sb.toString());
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        input();
+    }
+
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+}
